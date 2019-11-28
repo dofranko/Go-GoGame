6d38e5e5f6d5ce4f9550aea2284ab79b6d76eee1
Frankoo <dominik-frankowski@protonmail.com>
2019-11-28 12:00:32 +0100

    czystki

---
+++ Game/src/main/java/Go/Server.java
@@ -0,0 +1,63 @@
+package Go;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+  private ServerSocket server = null;
+  private Socket client = null;
+  private BufferedReader br;
+  private PrintWriter pw;
+  private RequestHandler threadList[] = new RequestHandler[2];
+  private Socket clientList[] = new Socket[2];
+
+  Server() {
+    try {
+      server = new ServerSocket(2137);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Brak połączenia na porcie 44444");
+      System.exit(-1);
+    }
+  }
+
+  public final void listenSocket() {
+    int counter = 0;
+    while (true) {
+      try {
+        System.out.println(counter);
+        if (counter <= 2) {
+          client = server.accept();
+          clientList[counter] = client;
+          //br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+          //pw = new PrintWriter(client.getOutputStream(), true);
+          threadList[counter] = new RequestHandler(client);
+          threadList[counter].start();
+          counter++;
+
+        } else {
+          System.out.println("Brak możliwości połączenia");
+          System.exit(1);
+        }
+      } catch (IOException e) {
+        System.out.println("Nieudane połączenie: 4444");
+        System.exit(-1);
+
+      }
+    }
+
+  }
+
+  protected final void finalize() {
+    try {
+      // client.close();
+      server.close();
+    } catch (IOException e) {
+      System.out.println("Nie udało się zamknąć");
+      System.exit(-1);
+    }
+  }
+
+}
---
+++ Game/src/test/java/Go/HandlerTest.java
@@ -0,0 +1,17 @@
+package Go;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HandlerTest {
+
+  @Test
+  public void test() {
+    Server s = new Server();
+    s.listenSocket();
+
+
+  }
+
+}
