7ff053bc43a67578824c9d5a988d75b9c643c024
krzysztof-szymaniak <krzysztof.szymaniak1004@gmail.com>
2019-12-15 00:32:11 +0100

    Drobny refactoring TheGame: przeniesienie gameState do Board, gettery i settery dla potrzebnych atrybutow, pozbycie sie niepotrzebnych tablic i map. Dodano mechanizm akceptacji zaznaczonych kamieni i terytorium, trzeba jeszcze to tylko zintegrować w całość

--- Game/src/test/java/Go/GameMaker/TheGameTest.java
+++ Game/src/test/java/Go/GameMaker/TheGameTest.java
@@ -15,15 +15,7 @@ public class TheGameTest {
 		assertEquals("Succes;White", game.addPlayer("playerTwo"));
 		assertEquals("Succes;Black", game.addPlayer("playerThree"));
 		assertEquals("Succes;White", game.addPlayer("playerFour"));
-		int boardID = game.getBoardsID().get("playerOne");
-		assertEquals(0, boardID);
-		boardID = game.getBoardsID().get("playerTwo");
-		assertEquals(0, boardID);
-		boardID = game.getBoardsID().get("playerThree");
-		assertEquals(1, boardID);
-		
-		
-		
+				
 	}
 	
 	@Test
@@ -31,18 +23,19 @@ public class TheGameTest {
 		TheGame game = TheGame.getInstance(); //ten sam singleton
 		String command = "playerOne,2,2";
 		
-		assertEquals("Black", game.whoseMove("playerOne").split(";")[0]);
-		assertEquals("0", game.makeMove(command).split(";")[0]); //pierwsza liczba ze stringa zwrotnego
-		assertEquals(Markers.BLACK.asChar(), game.getBoard(0).getBoard()[2][2]);
-		assertEquals("White", game.whoseMove("playerOne").split(";")[0]);
+		assertEquals("Black", game.getGameState("playerOne").split(";")[0]);
+		assertEquals("0", game.makeMove(command).split(";")[0]); 
+		assertEquals(Markers.BLACK.asChar(), game.getBoard("playerOne").getBoard()[2][2]);
+		assertEquals(Markers.BLACK.asChar(), game.getBoard("playerTwo").getBoard()[2][2]);
+		assertEquals("White", game.getGameState("playerOne").split(";")[0]);
 		
 		command = "playerTwo,2,3";
 		assertEquals("0", game.makeMove(command).split(";")[0]);
 		assertEquals("NotYrMove", game.makeMove(command).split(";")[0]);
-		assertEquals(Markers.WHITE.asChar(), game.getBoard(0).getBoard()[2][3]);
+		assertEquals(Markers.WHITE.asChar(), game.getBoard("playerOne").getBoard()[2][3]);
 		
 		command = "playerOne,2,3";
-		assertEquals("Black", game.whoseMove("playerOne").split(";")[0]);
+		assertEquals("Black", game.getGameState("playerOne").split(";")[0]);
 		assertEquals("IllegalMove", game.makeMove(command).split(";")[0]); //illegal move
 		
 	}
